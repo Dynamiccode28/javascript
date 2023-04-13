0286705b6629760f990635756b7da058ef82946c
Adarsh <28adarshdhangar@gmail.com>
2023-04-14 02:10:08 +0530

    Operator in javascript

---
+++ operators.js
@@ -0,0 +1,26 @@
+let a=5;
+let b=5;
+//Arithmetic operators
+console.log("a+b : ",a+b)
+console.log("a-b : ",a-b)
+console.log("a*b : ",a*b)
+console.log("a**b : ",a**b)
+console.log("a/b : ",a/b)
+console.log("a%b : ",a%b)
+
+//Logical operators
+console.log("a<=b : ",a<=b)
+let num1=4
+let num2=4
+console.log(num1==num2)//freehand
+num2="4"
+console.log(num1===num2)//strick check
+console.log(num1!=num2)
+
+//Assignment operators
+1.+=
+2.-=
+3.*=
+4./=
+5.%=
+6.**=
