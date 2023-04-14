5bf8a61b6b07ff77199cd41c15b7e7e1e6aaceeb
Adarsh <28adarshdhangar@gmail.com>
2023-04-15 01:31:58 +0530

    added conditonal statements

--- expression.js
+++ expression.js
@@ -1,3 +1,24 @@
 //Conditional statement
+//if statement
+//if .....else statement
+
 let a= prompt("tell me whats your age : ")
-a = parseInt()
+a = parseInt(a)
+if(a>0){
+    alert("Valid age")
+}
+else{
+    alert("invalid age")
+}
+
+//if....elseif...else statement
+if(a<15)
+{
+    alert("you are not allowed in party")
+}
+else if(15<a<50){
+    alert("show me your invitation")
+}
+else{
+    alert("invalid entry")
+}
