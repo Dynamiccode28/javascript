f773fbb9e2eba15523adb00a6089f8c6c74cfa9b
Adarsh <28adarshdhangar@gmail.com>
2023-04-16 10:04:39 +0530

    For_loop

---
+++ for_loop.js
@@ -0,0 +1,21 @@
+let a=prompt("enter a number upto which you want to compute sum : ")
+a=parseInt(a)
+let sum=0;
+for(let i=0;i<a;i++){
+  sum+=(i+1)
+}
+console.log(sum)
+
+//for in loop
+let obj={
+  Adarsh:50,shivam:48,
+  yash:50,kartik:68,shiv:50
+}
+for(let a in obj){
+  console.log(a+" : "+obj[a])
+}
+
+//for of loop : iterate over input
+for(let i of "ADARSH"){
+  console.log(i)
+}
