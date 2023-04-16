3d0b7f7b3a524270e3a34465d48cbba634111ff4
Adarsh <28adarshdhangar@gmail.com>
2023-04-16 10:11:07 +0530

    while & do_while loop

---
+++ while_loop.js
@@ -0,0 +1,13 @@
+//1.While loop...........
+let i=1
+while(i<=10){
+    console.log(i)
+    i++
+}
+/*------------------------------------------------------------------------------ */
+
+//2.do_while loop : execute for at least one time
+let a=10;
+do{
+    console.log("I'm do block")
+}while(a<2)//though condition is false still it will execute once
