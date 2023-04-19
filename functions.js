2c9da90e384b8f1cb623c896d880327ee1948709
Adarsh <28adarshdhangar@gmail.com>
2023-04-19 23:50:18 +0530

    Functions

---
+++ functions.js
@@ -0,0 +1,16 @@
+function average(num1,num2){
+    console.log("Function average get called")
+    return (num1+num2)/2;
+}
+
+const sum = (num1,num2)=>{
+    return num1+num2;
+}
+
+const Greeting =()=>{
+    console.log("Hey cody.......")
+}
+
+console.log("Avg. of 10 & 20 : ",average(10,20))
+console.log("sum of numbers : ",sum(5,5))
+Greeting()
